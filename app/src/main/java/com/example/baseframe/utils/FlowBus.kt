03122e2539375dda8e529323797ccbd0dc0274f9
lifuyuan123 <446960231@qq.com>
2022-06-16 17:11:28 +0800

    新增flowbus

---
+++ app/src/main/java/com/example/baseframe/utils/FlowBus.kt
@@ -0,0 +1,95 @@
+package com.example.baseframe.utils
+
+import androidx.lifecycle.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import timber.log.Timber
+
+/**
+ * @Author:  admin
+ * @Date:  2022/6/16-17:02
+ * @describe:  flowBus
+ */
+object FlowBus {
+    private val busMap = mutableMapOf<String, EventBus<*>>()
+    private val busStickMap = mutableMapOf<String, StickEventBus<*>>()
+
+    @Synchronized
+    fun <T> with(key: String): EventBus<T> {
+        var eventBus = busMap[key]
+        if (eventBus == null) {
+            eventBus = EventBus<T>(key)
+            busMap[key] = eventBus
+        }
+        return eventBus as EventBus<T>
+    }
+
+    @Synchronized
+    fun <T> withStick(key: String): StickEventBus<T> {
+        var eventBus = busStickMap[key]
+        if (eventBus == null) {
+            eventBus = StickEventBus<T>(key)
+            busStickMap[key] = eventBus
+        }
+        return eventBus as StickEventBus<T>
+    }
+
+    //真正实现类
+    open class EventBus<T>(private val key: String) : LifecycleObserver {
+
+        //私有对象用于发送消息
+        private val _events: MutableSharedFlow<T> by lazy {
+            obtainEvent()
+        }
+
+        //暴露的公有对象用于接收消息
+        val events = _events.asSharedFlow()
+
+        open fun obtainEvent(): MutableSharedFlow<T> = MutableSharedFlow(0, 1, BufferOverflow.DROP_OLDEST)
+
+        //主线程接收数据
+        fun register(lifecycleOwner: LifecycleOwner, action: (t: T) -> Unit) {
+            lifecycleOwner.lifecycle.addObserver(this)
+            lifecycleOwner.lifecycleScope.launch {
+                events.collect {
+                    try {
+                        action(it)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                        Timber.e("FlowBus - Error:$e")
+                    }
+                }
+            }
+        }
+
+        //协程中发送数据
+        suspend fun post(event: T) {
+            _events.emit(event)
+        }
+
+        //主线程发送数据
+        fun post(scope: CoroutineScope, event: T) {
+            scope.launch {
+                _events.emit(event)
+            }
+        }
+
+        //自动销毁
+        @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+        fun onDestroy() {
+            Timber.e("FlowBus - 自动onDestroy")
+            val subscriptCount = _events.subscriptionCount.value
+            if (subscriptCount <= 0)
+                busMap.remove(key)
+        }
+    }
+
+    class StickEventBus<T>(key: String) : EventBus<T>(key) {
+        override fun obtainEvent(): MutableSharedFlow<T> = MutableSharedFlow(1, 1, BufferOverflow.DROP_OLDEST)
+    }
+
+}
