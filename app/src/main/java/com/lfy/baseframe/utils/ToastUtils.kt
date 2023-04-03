de06987f85e2ba7a8c8546aee352a1a22b24be39
lifuyuan123 <446960231@qq.com>
2023-04-03 15:53:14 +0800

    修复部分机型Toast has already been added to the window manager问题

--- app/src/main/java/com/lfy/baseframe/utils/ToastUtils.kt
+++ app/src/main/java/com/lfy/baseframe/utils/ToastUtils.kt
@@ -1,7 +1,11 @@
 package com.lfy.baseframe.utils
 
+import android.content.Context
+import android.view.View
+import android.view.WindowManager
 import android.widget.Toast
 import com.lfy.baseframe.app.App
+import timber.log.Timber
 
 /**
  * @Author admin
@@ -21,6 +25,10 @@ object ToastUtils {
     }
 
     fun toast(msg: Any?) {
+
+        //解决Android Toast has already been added to the window manager
+        clearDying(App.context?.applicationContext!!)
+
         val nowTime = System.currentTimeMillis()
         if (oldMsg == msg.toString()) {
             if (nowTime - clickTime > 1800) {
@@ -33,6 +41,49 @@ object ToastUtils {
             toast.show()
             clickTime = nowTime
             oldMsg = msg.toString()
+        }
+    }
+
+    /**
+     * 解决Android Toast has already been added to the window manager
+     */
+    private fun clearDying(context: Context) {
+        try {
+            val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+            Timber.e("HMToast clearDying: $windowManager")
+            //反射拿到WindowManagerGlobal
+            val mGlobal = windowManager.javaClass.getDeclaredField("mGlobal")
+            mGlobal.isAccessible = true
+            Timber.e("HMToast clearDying: $mGlobal")
+            //反射拿到WindowManagerGlobal#mViews
+            val mViewsField = mGlobal.type.getDeclaredField("mViews")
+            val mRootsField = mGlobal.type.getDeclaredField("mRoots")
+            mViewsField.isAccessible = true
+            mRootsField.isAccessible = true
+            val mViews: ArrayList<View> = mViewsField.get(mGlobal.get(windowManager)) as ArrayList<View>
+            if (mViews.isEmpty()) {
+                return
+            }
+            val hookViews: ArrayList<View> = ArrayList(mViews.size)
+            val hookRoots :ArrayList<Any> = ArrayList(mViews.size)
+            Timber.e("HMToast clearDying: %s", mViews.size)
+            for (i in 0 until mViews.size) {
+                //清理parent为null的view
+                if (mViews[i].parent != null) {
+                    hookViews.add(mViews[i])
+                    hookRoots.add((mRootsField.get(mGlobal.get(windowManager)) as ArrayList<View>)[i])
+                }
+            }
+            Timber.e("HMToast clearDying: hookViews%s", hookViews.size)
+            if (mViews.size == hookViews.size) {
+                //没有脏view
+                return
+            }
+            mViewsField.set(mGlobal.get(windowManager), hookViews)
+            mRootsField.set(mGlobal.get(windowManager), hookRoots)
+        } catch (e: Exception) {
+            Timber.e("HMToast clearDying: %s", e.message)
+            e.printStackTrace()
         }
     }
 
