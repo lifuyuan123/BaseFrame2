d1a8728c9f3d698646cf388c76ffd6f2f7b66f25
lifuyuan123 <446960231@qq.com>
2023-09-15 11:30:07 +0800

    修改沉浸式版本号

--- app/src/main/java/com/company/baseframe/app/LifecycleCallbacks.kt
+++ app/src/main/java/com/company/baseframe/app/LifecycleCallbacks.kt
@@ -7,7 +7,6 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.gyf.immersionbar.ImmersionBar
-import com.gyf.immersionbar.SupportRequestManagerFragment
 import com.lfy.baselibrary.ActivityManager
 import timber.log.Timber
 
@@ -59,47 +58,5 @@ object LifecycleCallbacks {
             Timber.e("onActivityDestroyed:  ${activity.localClassName}")
 
         }
-
     }
-
-    /**
-     * fragment监听管理
-     */
-    val fragmentLifecycleCallbacks = object : FragmentManager.FragmentLifecycleCallbacks() {
-        override fun onFragmentAttached(fm: FragmentManager, f: Fragment, context: Context) {
-            super.onFragmentAttached(fm, f, context)
-            if (f !is SupportRequestManagerFragment)
-            Timber.e("栈中添加的fragment：onFragmentAttached  ${f::class.java.canonicalName}")
-        }
-
-        override fun onFragmentCreated(
-            fm: FragmentManager,
-            f: Fragment,
-            savedInstanceState: Bundle?
-        ) {
-            super.onFragmentCreated(fm, f, savedInstanceState)
-        }
-
-        override fun onFragmentStarted(fm: FragmentManager, f: Fragment) {
-            super.onFragmentStarted(fm, f)
-        }
-
-        override fun onFragmentResumed(fm: FragmentManager, f: Fragment) {
-            super.onFragmentResumed(fm, f)
-            ActivityManager.instance.getTopActivity()?.let {
-                ImmersionBar.with(it)
-                    .statusBarDarkFont(true)
-                    .init()
-            }
-
-        }
-
-        override fun onFragmentDestroyed(fm: FragmentManager, f: Fragment) {
-            super.onFragmentDestroyed(fm, f)
-            if (f !is SupportRequestManagerFragment)
-            Timber.e("栈中添加的fragment：  onFragmentDestroyed  ${f::class.java.canonicalName}")
-        }
-    }
-
-
 }
